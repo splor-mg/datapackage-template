eeec59986c1ab0e465fe84333b65b3c4806cf5d2
Francisco Júnior <fjunior.alves.oliveira@gmail.com>
2023-06-28 14:40:26 -0300

    adiciona etapa de build

--- Makefile
+++ Makefile
@@ -1,11 +1,11 @@
-.PHONY: all transform check
+.PHONY: all transform check build
 
 EXT = txt
 OUTPUT_DIR = build
 RESOURCE_NAMES := $(shell yq e '.resources[].name' datapackage.yaml)
 BUILD_FILES := $(addsuffix .csv,$(addprefix build/,$(RESOURCE_NAMES)))
 
-all: transform check
+all: transform check build
 
 transform: $(BUILD_FILES)
 
@@ -19,3 +19,6 @@ checks-python:
 
 checks-rstats:
 	Rscript checks/rstats/testthat.R
+
+build:
+	python scripts/build.py datapackage.json $(OUTPUT_DIR)
---
+++ scripts/build.py
@@ -0,0 +1,19 @@
+from pathlib import Path
+from frictionless import Package, Schema, transform, steps
+from datetime import datetime
+import typer
+
+descriptor = 'datapackage.json'
+
+def main(descriptor: str, output_path: Path):
+    package = Package(descriptor)
+    package.custom['updated_at'] = datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
+    for resource in package.resources:
+        schema = Schema.describe(resource.path)
+        resource.schema = schema
+        resource.infer(stats=True)
+        resource = transform(resource, steps=[steps.resource_update(name = resource.name, descriptor = {'path': f'{resource.name}.csv'})])
+    package.to_json(Path(output_path, 'datapackage.json'))
+    
+if __name__ == '__main__':
+    typer.run(main)
