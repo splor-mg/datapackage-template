5d2307c891586011c503be00e43e302615a0dcc0
Francisco Júnior <fjunior.alves.oliveira@gmail.com>
2023-06-28 11:43:28 -0300

    remove unused files

--- scripts/load.py
+++
@@ -1,7 +0,0 @@
-from frictionless import Package
-
-package = Package('datapackage.yaml')
-package.dereference()
-
-package.to_json('build/datapackage.json')
-package.publish(f'sqlite:///build/{package.name}.db')
